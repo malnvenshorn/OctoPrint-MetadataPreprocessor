77c28cae782b89569597656bf8e32dffec31bd1e
malnvenshorn <malnvenshorn@gmail.com>
2017-11-09 00:52:30 +0100

    Add sanity checks and exception handling

--- octoprint_metadatapreprocessor/__init__.py
+++ octoprint_metadatapreprocessor/__init__.py
@@ -42,7 +42,7 @@ class MetadataPreprocessorPlugin(octoprint.plugin.EventHandlerPlugin):
         analysis = self.read_metadata_from_file(path_on_disk)
 
         if analysis is None:
-            self._logger.info("No metadata found, requeuing {entry} for analysis by octoprint"
+            self._logger.info("Requeuing {entry} for analysis by octoprint"
                               .format(entry=queue_entry))
             self.queue.append(queue_entry)
             self._analysis_queue.enqueue(queue_entry, high_priority=True)
@@ -59,33 +59,44 @@ class MetadataPreprocessorPlugin(octoprint.plugin.EventHandlerPlugin):
             line = strip_comment(file_stream.readline())
 
             if not line.startswith("OCTOPRINT_METADATA"):
-                return None
+                self._logger.info("Metadata tag not found at the beginning of the file")
+                return
 
             metadata = []
 
+            max_lines = 100
+            line_no = 1
+
             for line in file_stream:
+                line_no += 1
                 line = strip_comment(line)
                 if line.startswith("OCTOPRINT_METADATA_END"):
                     break
+                elif line_no >= max_lines:
+                    self._logger.warn("Expecting metadata end tag within {no} lines".format(no=max_lines))
+                    return
                 else:
                     metadata.append(line)
 
-            analysis = yaml.safe_load("".join(metadata))
-
-            result = dict()
-            result["printingArea"] = analysis["printing_area"]
-            result["dimensions"] = analysis["dimensions"]
-            if analysis["total_time"]:
-                result["estimatedPrintTime"] = analysis["total_time"] * 60
-            if analysis["extrusion_length"]:
-                result["filament"] = dict()
-                for i in xrange(len(analysis["extrusion_length"])):
-                    result["filament"]["tool%d" % i] = {
-                        "length": analysis["extrusion_length"][i],
-                        "volume": analysis["extrusion_volume"][i]
-                    }
-
-            return result
+            try:
+                analysis = yaml.safe_load("".join(metadata))
+                result = dict()
+                result["printingArea"] = analysis["printing_area"]
+                result["dimensions"] = analysis["dimensions"]
+                if analysis["total_time"]:
+                    result["estimatedPrintTime"] = analysis["total_time"] * 60
+                if analysis["extrusion_length"]:
+                    result["filament"] = dict()
+                    for i in xrange(len(analysis["extrusion_length"])):
+                        result["filament"]["tool%d" % i] = {
+                            "length": analysis["extrusion_length"][i],
+                            "volume": analysis["extrusion_volume"][i]
+                        }
+                return result
+            except yaml.YAMLError as e:
+                self._logger.warn("Malformed metadata: {error}".format(error=str(e)))
+            except KeyError as e:
+                self._logger.warn("Missing property {key} in metadata".format(key=str(e)))
 
     # Softwareupdate hook
 
