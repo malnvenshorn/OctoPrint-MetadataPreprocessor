2d95c0a03188cae5a180a32d12b6719394561ddf
malnvenshorn <malnvenshorn@gmail.com>
2017-11-08 22:27:24 +0100

    Add missing copyright

--- analysis/analysis.py
+++ analysis/analysis.py
@@ -1,6 +1,10 @@
 # coding=utf-8
 from __future__ import absolute_import
 
+__author__ = "Sven Lohrmann <malnvenshorn@gmail.com> based on work from Gina Häußge <osd@foosel.net>"
+__license__ = "GNU Affero General Public License http://www.gnu.org/licenses/agpl.html"
+__copyright__ = "Copyright (C) 2017 Sven Lohrmann - Released under terms of the AGPLv3 License"
+
 import shutil
 import time
 import yaml
